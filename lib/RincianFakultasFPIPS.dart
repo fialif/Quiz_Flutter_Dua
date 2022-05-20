cca79502df6211e901c8af643c2b008e15b8d99c
fialif <74746311+fialif@users.noreply.github.com>
2022-05-20 17:56:24 +0700

    Update RincianFakultasFPIPS.dart

--- lib/RincianFakultasFPIPS.dart
+++ lib/RincianFakultasFPIPS.dart
@@ -9,7 +9,13 @@ class RincianFakultasFPIPS extends StatelessWidget {
         title: const Text('Rincian Fakultas'),
       ),
       body: Center(
-        child: Text("rincian fakultas"),
+        child: Column( mainAxisSize: MainAxisSize.min,
+          children: [
+            Image.network('https://flutter.github.io/assets-for-api-docs/assets/widgets/owl-2.jpg', width: 100),
+            Text("FPIPS"),
+            Text("Faculty of Social Sciences Education, Universitas Pendidikan Indonesia,is led by a Dean who is assisted by Vice Dean for Academic Affairs, Vice Dean for Finance and HR, and Vice Dean for Student Affairs. They are also supported by 4 Heads and Secretaries of Department, 8 Heads of Study Programs, and supported by administrative officers, namely: Head of Academic and Student Administration; Head of Finance and HR, and Head of Assets, Facilities and ICT Subdivision.")
+        ],
+        ),
       ),
     );
   }
