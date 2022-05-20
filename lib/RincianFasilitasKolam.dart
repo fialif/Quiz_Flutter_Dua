142b911b4e66e4858a005fea0b7c7a831302213e
fialif <74746311+fialif@users.noreply.github.com>
2022-05-20 17:59:44 +0700

    Update RincianFasilitasKolam.dart

--- lib/RincianFasilitasKolam.dart
+++ lib/RincianFasilitasKolam.dart
@@ -9,7 +9,13 @@ class RincianFasilitasKolam extends StatelessWidget {
         title: const Text('Rincian Fasilitas'),
       ),
       body: Center(
-        child: Text("rincian fakultas"),
+        child: Column( mainAxisSize: MainAxisSize.min,
+          children: [
+            Image.network('https://flutter.github.io/assets-for-api-docs/assets/widgets/owl-2.jpg', width: 100),
+            Text("Kolam Renang"),
+            Text("Gymnasium merupakan salah satu gedung yang dikelola sepenuhnya oleh UPT Pusat Olahraga Universitas Pendidikan Indonesia. Gedung gymnasium sebagai center atau pusat dari kegiatan kelembagaan seperti penerimaan mahasiswa baru atau wisudaan. Serta gedung Gymnasium biasa digunakan Untuk kepentingan perkuliahan, kegiatan olahraga maupun acara-acara kemahasiswaan.")
+        ],
+        ),
       ),
     );
   }
